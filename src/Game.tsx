f18710b8241c165c4e7760d04e15ed99393e0522
Gregory Sanchez <gregory@dev.com>
2024-06-12 15:45:25 -0300

    Darkrtheme for material ui

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,16 @@
 import { Card, IconButton, Stack, Typography } from '@mui/material'
 import { type Question as QuestionType } from './types'
 import { useQuestionsStore } from './store/Questions'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant='outlined'>
+    <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left' }}>
       <Typography variant='h5'>{info.question}</Typography>
+      <SyntaxHighlighter language='javascript' style={gradientDark}>
+        {info.code}
+      </SyntaxHighlighter>
     </Card>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 )
