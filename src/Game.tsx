c4b823ab4f0affd21110350cc2002d66d83a6da7
Gregory Sanchez <gregory@dev.com>
2024-06-12 17:19:05 -0300

    intify is answer selected is correct or not

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,16 +1,65 @@
-import { Card, IconButton, Stack, Typography } from '@mui/material'
+import {
+  Card,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography,
+} from '@mui/material'
 import { type Question as QuestionType } from './types'
 import { useQuestionsStore } from './store/Questions'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
+// esta funcion getBackgroundColor, al estar fuera del componente, solo se renderiza una vez, si estuviese dentro, se rendereizaría cuando el componente le toque
+const getBackgroundColor = (info: QuestionType, index: number) => {
+  const { userSelectedAnswer, correctAnswer } = info
+  // user no ha seleccionado nada
+  if (userSelectedAnswer == null) return 'transparent'
+  // user ya seleccionó pero la solucion es incorrecta
+  if (index !== correctAnswer && index !== userSelectedAnswer)
+    return 'transparent'
+  // si es la solucion correcta
+  if (index === correctAnswer) return 'green'
+  //si es la solucion del usuario pero no es correcta
+  if (index === userSelectedAnswer) return 'red'
+
+  return 'transparent'
+}
+
 const Question = ({ info }: { info: QuestionType }) => {
+  const selectAnswer = useQuestionsStore((state) => state.selectAnswer)
+
+  const handleClick = (questionId: number, answerIndex: number) => {
+    selectAnswer(questionId, answerIndex)
+  }
+  console.log(info)
   return (
-    <Card variant='outlined' sx={{ bgcolor: '#222', p: 2, textAlign: 'left' }}>
+    <Card
+      variant='outlined'
+      sx={{ bgcolor: '#222', p: 2, textAlign: 'left', marginTop: '4' }}
+    >
       <Typography variant='h5'>{info.question}</Typography>
       <SyntaxHighlighter language='javascript' style={gradientDark}>
         {info.code}
       </SyntaxHighlighter>
+      <List sx={{ bgcolor: '#333' }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton
+              sx={{
+                backgroundColor: getBackgroundColor(info, index),
+              }}
+              onClick={() => handleClick(info.id, index)}
+              disabled={info.userSelectedAnswer !== undefined}
+            >
+              <ListItemText primary={answer} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   )
 }
--- src/store/Questions.tsx
+++ src/store/Questions.tsx
@@ -1,10 +1,12 @@
 import { create } from 'zustand'
 import { type Question } from '../types'
+import confetti from 'canvas-confetti'
 
 interface State {
   questions: Question[]
   currentQuestion: number
   fetchQuestions: (limit: number) => Promise<void>
+  selectAnswer: (questionId: number, answerIndex: number) => void
 }
 
 export const useQuestionsStore = create<State>((set, get) => {
@@ -17,6 +19,29 @@ export const useQuestionsStore = create<State>((set, get) => {
       const json = await res.json()
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
       set({ questions })
+    },
+
+    selectAnswer: (questionId: number, answerIndex: number) => {
+      const { questions } = get()
+      // Usar structuredClone para clonar el objeto
+      const newQuestions = structuredClone(questions)
+      // encontramos el indice de la pregunta
+      const questionIndex = newQuestions.findIndex((q) => q.id === questionId)
+      // obtenemos la informacion completa de la pregunta
+      const questionInfo = newQuestions[questionIndex]
+      // chequeamos si el user ha selecionado la respuesta correcta
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+
+      if (isCorrectUserAnswer) confetti()
+
+      // ahora actualizamos el estado. Mutando la copia que hemos hecho con el structuredClone
+      newQuestions[questionIndex] = {
+        ...questionInfo,
+        isCorrectUserAnswer,
+        userSelectedAnswer: answerIndex,
+      }
+      // Actualizamos el estado
+      set({ questions: newQuestions })
     },
   }
 })
